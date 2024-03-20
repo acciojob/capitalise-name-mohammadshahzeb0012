612e8d1c74d484dc015df4061cf1d3442006c1ed
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-03-20 13:23:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+    const input = document.querySelector('#fname')
+    input.addEventListener('blur',()=>{
+      const value = input.value.toUpperCase()
+      if(value){
+        input.value = value;
+      }
+      else{
+        alert('please enter something')
+      }
+    })
